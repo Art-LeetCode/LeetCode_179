63b23755fada3e5954df643ed3b10ccf2ca68df1
art <partypeopleland@gmail.com>
2019-05-10 17:33:45 +0800

    add case

--- src/solution.js
+++ src/solution.js
@@ -1,7 +1,16 @@
 class Solution {
   constructor() {}
-  GetLargestNumber(nums) {
-    return "210";
+  GetLargestNumber(numbers) {
+    let sortList = numbers.sort(function(x, y) {
+      let xFirst = x.toString() + y.toString();
+      let yFirst = y.toString() + x.toString();
+      return yFirst - xFirst;
+    });
+    let result = sortList.reduce((a, b) => {
+      if (a == b) return a.toString();
+      return a.toString() + b.toString();
+    });
+    return result;
   }
 }
 
--- test/solution.test.js
+++ test/solution.test.js
@@ -12,4 +12,32 @@ describe("Get Largest Number", () => {
     var actual = sut.GetLargestNumber(nums);
     expected.should.be.equal(actual);
   });
+  it("[0,0] should be 0", () => {
+    var sut = new Solution();
+    var expected = "0";
+    var nums = [0, 0];
+    var actual = sut.GetLargestNumber(nums);
+    expected.should.be.equal(actual);
+  });
+  it("[3, 30, 34, 5, 9] should be 9534330", () => {
+    var sut = new Solution();
+    var expected = "9534330";
+    var nums = [3, 30, 34, 5, 9];
+    var actual = sut.GetLargestNumber(nums);
+    expected.should.be.equal(actual);
+  });
+  it("[121,12] should be 12121", () => {
+    var sut = new Solution();
+    var expected = "12121";
+    var nums = [121, 12];
+    var actual = sut.GetLargestNumber(nums);
+    expected.should.be.equal(actual);
+  });
+  it("[824, 938, 1399, 5607, 6973, 5703, 9609, 4398, 8247] should be 9609938824824769735703560743981399", () => {
+    var sut = new Solution();
+    var expected = "9609938824824769735703560743981399";
+    var nums = [824, 938, 1399, 5607, 6973, 5703, 9609, 4398, 8247];
+    var actual = sut.GetLargestNumber(nums);
+    expected.should.be.equal(actual);
+  });
 });
